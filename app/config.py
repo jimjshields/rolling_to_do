7712b982eb2a1d2c53eed000db2a7631e2364b3e
jimjshields <jim.j.shields@gmail.com>
2015-01-13 14:14:10 -0500

    secret key addition

---
+++ app/config.py
@@ -0,0 +1,28 @@
+import sys
+import os.path
+from app import app
+
+# taken from http://flask.pocoo.org/snippets/104/ - read a secret key from a file
+def install_secret_key(app, filename='secret_key'):
+    """Configure the SECRET_KEY from a file
+    in the instance directory.
+
+    If the file does not exist, print instructions
+    to create it from a shell with a random key,
+    then exit.
+
+    """
+    filename = os.path.join(app.instance_path, filename)
+    try:
+        app.config['SECRET_KEY'] = open(filename, 'rb').read()
+    except IOError:
+        print 'Error: No secret key. Create it with:'
+        if not os.path.isdir(os.path.dirname(filename)):
+            print 'mkdir -p', os.path.dirname(filename)
+        print 'head -c 24 /dev/urandom >', filename
+        sys.exit(1)
+
+# cross-site request forgery prevention
+WTF_CSRF_ENABLED = True
+# secret key
+SECRET_KEY = install_secret_key(app)
